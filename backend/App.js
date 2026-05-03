9680dc10cf9512e71e325b89199922d180bb60d8
manish3-4 <preetisikka553@gmail.com>
2026-05-04 02:09:00 +0530

    cors Issue Resolved

--- backend/App.js
+++ backend/App.js
@@ -1,33 +1,34 @@
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs');
-const cookieParser = require('cookie-parser');
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const cookieParser = require("cookie-parser");
+const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-
-
-app.use(cors({
-    origin: 'http://localhost:3000',  // Allow all origins
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://expensetrakr.netlify.app"], // Allow localhost and deployed frontend
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
-}));
+  }),
+);
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(cookieParser());
 
-
-readdirSync('./routes').map((route) => app.use('', require('./routes/' + route)))
+readdirSync("./routes").map((route) =>
+  app.use("", require("./routes/" + route)),
+);
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log(PORT);
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log(PORT);
+  });
+};
 
 server();
